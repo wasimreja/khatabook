bb58bf56c647bcd01bc8f4c3dd5999f8560d02f6
wasimreja <iwasimreja@gmail.com>
2022-07-02 02:42:31 +0530

    Add context and transaction functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h2 className="is-size-3 has-text-weight-bold" style={{ marginBottom: 20 }}>
-      Expense Tracker
+    <h2 className="is-size-3 has-text-weight-bold" style={{ textAlign: 'center', marginBottom: 20 }}>
+      Khatabook
     </h2>
   )
 }
